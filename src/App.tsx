ef429fc6459fb7037cb388fc275656c5e89f2ee6
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2023-02-21 01:23:54 -0300

    Feature: Setup do Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/theme/default'
+import { GlobalStyle } from './styles/global'
+
 export function App() {
-  return <div className="App"></div>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Portfolio VS Code</h1>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
---
+++ src/styles/theme/default.ts
@@ -0,0 +1,7 @@
+export const defaultTheme = {
+  primary: '#111111',
+  secondary: '#222222',
+  tertiary: '#333333',
+  accent: '#88cc44',
+  text: '#FFFFFF',
+} as const
