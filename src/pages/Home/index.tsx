9c1c4ea222cf7cfd291332c450be5362f15c1ffc
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2023-02-23 18:03:59 -0300

    Feature: Criado o conteudo da tela Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,38 @@
+import {
+  HelloText,
+  HomeContainer,
+  ImageContainer,
+  ImageRodrigo,
+  NameText,
+  PresentationContainer,
+} from './styles'
+import Typing from 'react-typing-animation'
+
 export function Home() {
-  return <div></div>
+  return (
+    <HomeContainer>
+      <PresentationContainer>
+        <HelloText>
+          <span>{'<'}</span>HelloWorld 😎 eu sou o
+        </HelloText>
+        <NameText>
+          Rodrigo Viegas &nbsp;<span>{'/>'}</span>
+        </NameText>
+        <Typing speed={100} loop={true}>
+          <span>Desenvolvedor Front-End</span>
+          <Typing.Delay ms={1000} />
+          <Typing.Backspace count={25} />
+          <span>Desenvolvedor Full-Stack</span>
+          <Typing.Delay ms={1000} />
+          <Typing.Backspace count={25} />
+          <span>UI/UX</span>
+          <Typing.Delay ms={1000} />
+          <Typing.Backspace count={25} />
+        </Typing>
+      </PresentationContainer>
+      <ImageContainer>
+        <ImageRodrigo src="https://i.imgur.com/8LNBW16.jpg" alt="Rodrigo" />
+      </ImageContainer>
+    </HomeContainer>
+  )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,1 +1,70 @@
 import styled from 'styled-components'
+import * as ScrollArea from '@radix-ui/react-scroll-area'
+
+export const HomeContainer = styled(ScrollArea.Root)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 16px;
+  width: 100%;
+  height: calc(100vh - 120px);
+  gap: 30px;
+
+  @media only screen and (max-device-width: 1200px) {
+    flex-direction: column;
+  }
+`
+
+export const PresentationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: end;
+  gap: 8px;
+  flex: 1;
+
+  @media only screen and (max-device-width: 1200px) {
+    justify-content: flex-end;
+    align-items: center;
+    flex: 0.5;
+  }
+`
+export const HelloText = styled.p`
+  font-size: 1.25rem;
+  font-weight: 400;
+  color: ${({ theme }) => theme.accent};
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  line-height: 16px;
+
+  span {
+    font-weight: 700;
+    font-size: 30px;
+    margin-top: 2px;
+  }
+`
+
+export const NameText = styled.p`
+  font-size: 2.625rem;
+  font-weight: 700;
+  color: ${({ theme }) => theme.text};
+  display: flex;
+
+  span {
+    color: ${({ theme }) => theme.accent};
+  }
+`
+
+export const ImageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  flex: 1;
+`
+
+export const ImageRodrigo = styled.img`
+  width: 300px;
+  border-radius: 50%;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+`
