2e6ad311e4811e8b14acbec6e137ef278e1c2df6
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2023-02-23 18:06:30 -0300

    Feature: Adicionado sombra no ProjectItem

--- src/pages/Projects/components/ProjectItem/styles.ts
+++ src/pages/Projects/components/ProjectItem/styles.ts
@@ -8,6 +8,7 @@ export const ProjectItemContainer = styled.div`
   background: ${({ theme }) => theme.primary};
   border-radius: 4px;
   gap: 1rem;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 `
 
 export const ProjectItemImage = styled.img`
