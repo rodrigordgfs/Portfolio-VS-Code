aa699b44f9e2b468506f19f512fcd58170f7a553
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2023-02-25 22:41:21 -0300

    Feature: Criado o conteudo da pagina de contato

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,3 +1,70 @@
+import { ContactContainer, ContactInfo, ContactList } from './styles'
+
 export function Contact() {
-  return <div></div>
+  return (
+    <ContactContainer>
+      <ContactInfo>
+        <h2>
+          Quer entrar em
+          <br />
+          contato comigo?
+        </h2>
+        <p>Basta clicar em um dos links</p>
+      </ContactInfo>
+      <ContactList>
+        <li>
+          .<span>redes-sociais</span> {'{'}
+        </li>
+        <li>
+          email:{' '}
+          <a href="mailto:rodrigordgfs@gmail.com">rodrigordgfs@gmail.com</a>;
+        </li>
+        <li>
+          github:{' '}
+          <a
+            target="_blank"
+            href="https://github.com/rodrigordgfs"
+            rel="noreferrer"
+          >
+            rodrigordgfs
+          </a>
+          ;
+        </li>
+        <li>
+          linkedin:{' '}
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/rodrigo-viegas-rodrigues-83315164/"
+            rel="noreferrer"
+          >
+            rodrigo-viegas-rodrigues
+          </a>
+          ;
+        </li>
+        <li>
+          instagram:{' '}
+          <a
+            target="_blank"
+            href="https://www.instagram.com/mr_shinodaa/"
+            rel="noreferrer"
+          >
+            @mr_shinodaa
+          </a>
+          ;
+        </li>
+        <li>
+          facebook:{' '}
+          <a
+            target="_blank"
+            href="https://www.facebook.com/MrShiinoda/"
+            rel="noreferrer"
+          >
+            rodrigo-shinoda
+          </a>
+          ;
+        </li>
+        <li>{'}'}</li>
+      </ContactList>
+    </ContactContainer>
+  )
 }
--- src/pages/Contact/styles.ts
+++ src/pages/Contact/styles.ts
@@ -1,1 +1,85 @@
 import styled from 'styled-components'
+import * as ScrollArea from '@radix-ui/react-scroll-area'
+
+export const ContactContainer = styled(ScrollArea.Root)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 16px;
+  width: 100%;
+  height: calc(100vh - 120px);
+
+  @media only screen and (max-device-width: 980px) {
+    flex-direction: column;
+  }
+`
+
+export const ContactInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: end;
+  justify-content: center;
+  height: 200px;
+  gap: 16px;
+  padding-right: 18px;
+
+  h2 {
+    font-size: 32px;
+    font-weight: 700;
+    text-align: end;
+  }
+
+  p {
+    color: ${({ theme }) => theme['gray-400']};
+    text-align: end;
+  }
+
+  @media only screen and (max-device-width: 980px) {
+    align-items: center;
+    gap: 8px;
+    padding-right: 0;
+    h2 {
+      text-align: center;
+    }
+
+    p {
+      text-align: center;
+    }
+  }
+`
+
+export const ContactList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  line-height: 160%;
+  font-size: 18px;
+  padding-left: 18px;
+  list-style-type: none;
+
+  li {
+    span {
+      color: ${({ theme }) => theme.accent};
+      font-weight: 700;
+    }
+  }
+
+  li:not(:first-child):not(:last-child) {
+    padding-left: 16px;
+  }
+
+  a {
+    color: ${({ theme }) => theme.accent};
+    font-weight: 700;
+    text-decoration: none;
+    cursor: pointer;
+
+    &:hover {
+      text-decoration: underline;
+    }
+
+    @media only screen and (max-device-width: 980px) {
+      padding-left: 0;
+    }
+  }
+`
